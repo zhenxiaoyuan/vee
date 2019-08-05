1c1e8bc23efa383df4062e43eaf8ca675ac2b075
zhenxiaoyuan <i_zhenhao@163.com>
2019-08-05 17:14:57 +0800

    modified README.md and vee.c .h

--- README.md
+++ README.md
@@ -1,1 +1,78 @@
 # vee
+
+### Contents
+
+1 socket
+1.1 declaration
+1.2 listen-svr_sock
+1.2.1 init
+1.2.2 bind
+1.2.3 listen
+1.2.4 set_nonblock
+1.3 accept-clt_sock
+1.3.1 accept
+1.3.2 set_nonblock
+1.4 implement of set_nonblock
+1.4.1 fcntl
+
+2 epoll
+2.1 create
+2.2 ctl
+*edge trigger*
+*oneshot*
+2.2.1 add
+2.2.2 mod
+2.2.3 del
+2.3 wait
+
+3 threadpool
+3.1 init
+3.2 add
+3.3 destroy
+3.4 task
+3.4.1 task_queue
+3.5 worker
+3.5.1 mutex
+3.5.2 cond
+
+4 timer
+4.1 init
+4.2 add
+4.3 del
+4.4 update
+4.5 find
+4.6 expire
+*relate to priority_queue*
+
+5 priority_queue
+5.1 init
+5.2 algorithm-min_heap
+5.3 add
+5.4 del
+
+6 rio
+6.1 buffered
+6.1.1 read
+6.1.2 readline
+6.2 unbuffered
+6.2.1 read
+6.2.2 write
+*ref-CSAPP*
+
+7 http
+7.1 request
+7.2 parse
+7.2.1 header
+7.2.2 body
+7.2.3 list
+
+8 config
+8.1 read the cfg file
+8.2 TODO: sig when remote close
+
+9 error and dbg
+9.1 error macro
+9.2 msg
+9.2.1 log_err
+9.2.2 log_info
+9.3 debug
--- src/vee.c
+++ src/vee.c
@@ -1,125 +1,124 @@
 /*
- * author   zhenxiaoyuan
- * email    i_zhenhao@163.com
+ *      Author:     Hao Zhen, <i_zhenhao@163.com>
  */
 
-#include "vee.h"
-#include "error.h"
+#define PORT_NUM 7777   // read in cfg file later
 
-#define MAX_BUF     256     /* buffer size for read and write */
-#define MAX_LISTEN  8
-#define MAX_EPOLL   64
-
-#define PORT_NUM    7777
-
-void set_nonblock(int);
-
-int
-main(int argc, char *argv[])
-{
-    // usage
+int main(int argc, char **argv) {
+    // TODO
+    // read_cfg_file()
+    // 1. listen_port
+    // 2. thread_num
+    // 3. ...
 
     // socket declaration
-    int sv_sock, cl_sock;                   // server and client socket
-    struct sockaddr_in sv_addr, cl_addr;    // server and client address
-    socklen_t addr_len;
-    int str_len, i;
-    char buf[MAX_BUF];
+    // TODO: change svr_sock to listenfd when extract later
+    int svr_sock, clt_sock;                 // socket
+    struct sockaddr_in svr_addr, clt_addr;  // address
+    socklen_t addr_len;     // use in accept
+    int str_len;     // str_len is the length in IO operation, move into IO operation.
+    char buf[MAX_BUF];  // read and write buffer, move into IO operation.
 
-    // server socket init
-    sv_sock = socket(AF_INET, SOCK_STREAM, 0);
-    bzero(&sv_addr, sizeof(sv_addr));
-    sv_addr.sin_family      = AF_INET;
-    sv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    sv_addr.sin_port        = htons(PORT_NUM);
+    // socket init
+    svr_sock = socket(AF_INET, SOCK_STREAM, 0);
+    bzero(&svr_addr, sizeof(svr_addr));
+    svr_addr.sin_family      = AF_INET;
+    svr_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    svr_addr.sin_port        = htons(PORT_NUM);
 
-    // bind server socket
-    if (bind(sv_sock, (struct sockaddr *) &sv_addr, sizeof(sv_addr)) == -1)
-        error_exit("bind");
+    // bind
+    if (bind(svr_sock, (struct sockaddr *) &svr_addr, sizeof(svr_addr)) == -1) {
+        err_sys("[main] bind error");
+        // TODO error.h and .c
+    }
 
-    // start listening
-    if (listen(sv_sock, MAX_LISTEN) == -1)
-        error_exit("listen");
-
-    // epoll declaration
+    // listen
+    if (listen(svr_sock, LISTENQ) == -1) {
+        err_sys("[main] listen error");
+    }
+    //
+    // set_nonblock
+    make_socket_non_block(svr_sock);
+    //
+    // epoll
+    // init
+    int epfd;
     struct epoll_event ev_list[MAX_EPOLL];
     struct epoll_event ev;
-    int epfd, ready;
+    // create
+    epfd = epoll_create1(0);    // TODO: use macro to control events_num
+    //
+    // init request
+    vee_http_request_t *request = (vee_http_request_t *)malloc(sizeof(vee_http_request_t));
+    vee_init_request_t(request, fd);
 
-    // epoll create
-    epfd = epoll_create1(0);
+    // add listenfd to epoll
+    ev.events = EPOLLIN | EPOLLET;    // if listenfd need EPOLLET and EPOLLONESHOT ?
+    ev.data.ptr = (void *)request;  // what's the structure of ev.data ?
+    // what's the diff between ev.data.fd and ev.data.ptr->fd
 
-    // set server socket to nonblock
-    set_nonblock(sv_sock);
-    // add server socket to epoll
-    ev.events = EPOLLIN;
-    ev.data.fd = sv_sock;
-    epoll_ctl(epfd, EPOLL_CTL_ADD, sv_sock, &ev);
+    vee_epoll_add(epfd, svr_sock, &ev);
+    // TODO: error control?
 
+    // thread pool used to do requests
+    // init thread pool
+    vee_threadpool_init();  // TODO: threadpool
+    // init timer, priority queue included in timer
+    vee_timer_init();       // TODO: timer
+    //
     while (1) {
-        // meaning of every param, especially the last one
-        ready = epoll_wait(epfd, ev_list, MAX_EPOLL, -1);
-        if (ready == -1) {
-            if (errno == EINTR)
-                continue;
-            else
-                error_exit("epoll_wait");
-        }
-
+        // find the minest timer for epoll_wait
+        time = vee_find_timer();
+        // epoll_wait
+        ready = vee_epoll_wait(epfd, ev_list, MAX_EPOLL, time);
+        // TODO: ready error control
+        // expire timers
+        vee_handle_expire_timers();
+        //
         for (int i = 0; i < ready; i++) {
-            // server socket - accept - add client socket to epoll
-            if (ev_list[i].data.fd == sv_sock) {
-                addr_len = sizeof(cl_addr);
-                cl_sock = accept(sv_sock, (struct sockaddr *) &cl_addr, &addr_len);
-                // set client socket to nonblock
-                set_nonblock(cl_sock);
-                ev.events = EPOLLIN | EPOLLET;  // use edge trigger
-                ev.data.fd = cl_sock;
-                epoll_ctl(epfd, EPOLL_CTL_ADD, cl_sock, &ev);
+            //vee_http_request_t request = (vee_http_request_t *)malloc(sizeof(vee_http_request_t));
+            vee_http_request_t request = (vee_http_request_t *)ev_list[i].data.ptr;
+            svr_sock = request->fd; // use infd at here later
+            // fd == listenfd
+            if (svr_sock == ev_list[i].data.fd) {
+                // accept clt_sock
+                clt_sock = accept(svr_sock, (struct sockaddr *) &cltaddr, &addrlen);
+                // set clt_sock to non block
+                make_socket_non_block(clt_sock);
+                // epoll_add
+                // is this influence the epoll_event when listenfd?
+                // maybe no influence: ev_list changed in kernel
 
-                log_info("connected client: %d", cl_sock);
+                // init request
+                vee_http_request_t *request = (vee_http_request_t *)malloc(sizeof(vee_http_request_t));
+                vee_init_request_t(request);
 
+                // add listenfd to epoll
+                ev.events = EPOLLIN | EPOLLET | EPOLLONSHOT; // why onshot?
+                ev.data.ptr = (void *)request;  // what's the structure of ev.data ?
+
+                vee_epoll_add(epfd, clt_sock, &ev);
+                vee_add_timer()
             } else {
-                // client socket
-                while (1) {
-                    // client socket - read
-                    str_len = read(ev_list[i].data.fd, buf, MAX_BUF);
-
-                    // str_len < 0 - judge EAGAIN
-                    // todo: read tlpi/chapter4
-                    if (str_len < 0) {
-                        if (errno == EAGAIN)
-                            break;
-                    }
-                    // str_len == 0 - read completed - close client socket and delete in epoll
-                    else if (str_len == 0) {
-                        close(ev_list[i].data.fd);
-                        epoll_ctl(epfd, EPOLL_CTL_DEL, ev_list[i].data.fd, NULL);
-                        log_info("closed client: %d", ev_list[i].data.fd);
-
-                        break;
-                    }
-                    // str_len > 0 - write
-                    else {
-                        write(ev_list[i].data.fd, buf, str_len);
-                    }
+                // exclude error event
+                if ((ev_list[i].events & EPOLLERR) ||
+                    (ev_list[i].events & EPOLLHUP) ||
+                    (!(ev_list[i].events & EPOLLIN))) {
+                    log_err(); // TODO: log
+                    close(fd);
+                    continue;
                 }
+                // add to threadpool to do request
+                vee_threadpool_add(tp, do_request, events[i].data.ptr);
+
             }
         }
     }
-    // close server socket and epoll fd
-    close(sv_sock);
-    close(epfd);
-
-    return 0;
 }
 
-void set_nonblock(int fd)
-{
-    int flag = fcntl(fd, F_GETFL);
-    fcntl(fd, F_SETFL, flag | O_NONBLOCK);
+// move to util.h later
+int make_socket_non_block(int socket) {
+    // TODO
 }
 
-// todo 
-// 0. resolve starving
-// 1. thread and threadpool
+
