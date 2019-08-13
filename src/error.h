193a923ecaaeabed7ab3a0237600837968be5f61
zhenxiaoyuan <i_zhenhao@163.com>
2019-08-13 14:17:03 +0800

    1. new error.c and error.h 2. modified epoll.c 3. modified Makefile

--- src/error.h
+++ src/error.h
@@ -1,6 +1,10 @@
 #ifndef ERROR_H
 #define ERROR_H
 
+/*
+* Nonfatal error related to a system call.
+* Print a message and return.
+*/
 void err_msg(const char *format, ...);
 
 #ifdef __GNUC__
@@ -14,34 +18,41 @@ terminate main() or some other non-void function. */
 #define NORETURN
 #endif
 
+/*
+* Fatal error related to a system call.
+* Print a message and terminate.
+*/
 void err_exit(const char *format, ...) NORETURN;
 
+/*
+* Fatal error related to a system call.
+* Doesn't flush standard output before printing the error message.
+* Terminates the process by calling _exit() instead of exit().
+*/
 void _err_exit(const char *format, ...) NORETURN;
 
-void err_exit_en(const char *format, ...) NORETURN;
+/*
+* Fatal error unrelated to a system call.
+* Print a message and terminate.
+*/
+void err_exit_en(int errnum, const char *format, ...) NORETURN;
+
+/*
+ * Standard error.
+ * Print a message and terminate.
+ */
+void fatal(const char *format, ...) NORETURN;
+
+/*
+ * Print string Usage: followed by the
+ * formatted output on standard error.
+ */
+void usage_err(const char *format, ...) NORETURN;
+
+/*
+ * Print string Command-line usage error: followed
+ * by the formatted output on standard error.
+ */
+void cmd_err(const char *format, ...) NORETURN;
 
 #endif
-/*
-#include <stdio.h>
-#include <string.h>
-#include <errno.h>
-#include <stdlib.h>
-
-
-#define VEE_OK     0
-#define VEE_ERROR  -1
-
-#define clean_errno() (errno == 0 ? "None" : strerror(errno))
-
-// todo: use err_num in threads
-#define log_err(M, ...) fprintf(stderr, "[ERROR] (%s %s - %s:%d: errno: %s) " M "\n", __DATE__, __TIME__,  __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
-
-#define log_info(M, ...) fprintf(stderr, "[INFO] (%s %s - %s:%d) " M "\n", __DATE__,  __TIME__, __FILE__, __LINE__, ##__VA_ARGS__)
-
-#define error_msg(M, ...) log_err(M "\n", ##__VA_ARGS__)
-
-#define error_exit(M, ...) { log_err(M "\n", ##__VA_ARGS__); exit(1); }
-
-// TODO - check
-*/
-
