8f1bccfb0dd5d1be782e77508a6dca3f8a8c0425
zhenxiaoyuan <i_zhenhao@163.com>
2019-10-31 10:43:42 +0800

    1. Modified Makefile     Add compile option for threadpool 2. Modified request.h     Delete some commented codes 3. Modified threadpool.c and threadpool.h     Add threadpool for vee, compiler passed

--- src/request.h
+++ src/request.h
@@ -6,11 +6,6 @@ typedef struct vee_conn_s {
     int epfd;   // Necessary?
 } vee_conn_t;
 
-//typedef struct vee_request_s {
-    //int fd;
-    //int epfd;   // Necessary?
-//} vee_request_t;
-
 vee_conn_t *vee_conn_init(int fd, int epfd);
 
 #endif  /* VEE_REQUEST_H */
